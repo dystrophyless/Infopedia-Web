1cb05d0b82ece47c218b5ac0321570d8eda65032
dystrophyless <romansokolov689@gmail.com>
2026-08-16 01:07:30 +0500

    chore(icons): remove unused custom assets
    - Delete seven unreferenced SVG assets.
    - Reduce the Figma icon barrel to its live profile export.

--- frontend/src/components/FigmaIcons.tsx
+++ frontend/src/components/FigmaIcons.tsx
@@ -1,7 +1,1 @@
-export { default as FigmaProfileIcon }            from '../assets/icons/profile.svg?react';
-export { default as FigmaStatBooksIcon }          from '../assets/icons/stat-books.svg?react';
-export { default as FigmaStatTopicsIcon }         from '../assets/icons/stat-topics.svg?react';
-export { default as FigmaStatTermsIcon }          from '../assets/icons/stat-terms.svg?react';
-export { default as FigmaFeatureSearchIcon }      from '../assets/icons/feature-search.svg?react';
-export { default as FigmaFeatureDescriptionIcon } from '../assets/icons/feature-description.svg?react';
-export { default as FigmaFeatureAnalyticsIcon }   from '../assets/icons/feature-analytics.svg?react';
+export { default as FigmaProfileIcon } from '../assets/icons/profile.svg?react';
